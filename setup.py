8baa28a0adfee3bd69152c69cf840aaffce8f8e4
Old Ed Turner1 <OldEdTurner1@Edwards-MacBook.home>
2018-12-31 17:46:30 -0500

    Init

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+__version__ = '0.0'
+
+with open("README.md") as f:
+    README = f.readlines()
+
+#with open("LICENSE") as f:
+#    LICENSE = f.readlines()
+
+LICENSE = ''
+
+REQUIREMENTS = ['pandas==0.23.4', 'numpy==1.15.4', 'numba==0.41.0']
+
+setup(
+    name='categorical_kneighbors',
+    packages=['categorical_kneighbors', ],
+    version=__version__,
+    install_requires=REQUIREMENTS,
+    author="Edward Turner",
+    author_email="edward.turnerr@gmail.com",
+    description=README,
+    license=LICENSE
+)
